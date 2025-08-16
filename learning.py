c1601311e91a2cea1d9f556c6569eb80bec7cc13
shatinz <shatincia@gmail.com>
2025-08-16 13:12:25 -0700

    learning

---
+++ learning.py
@@ -0,0 +1,32 @@
+'''
+    supervised-learning :
+      given a data set of inpu-output pairs. learn a function to map input to outputs.
+    
+   classification :
+      supervised learning task of learning a function mapping an input point to a discrete
+      category.
+    
+    nearest-neighber classification:
+      algorythm that, given an input ,chooses the class of the nearest data point to that input.
+
+    k-nearest-neighbor classification :
+      (k is presented for number of neighbors to be consider as near ) algorythm that, given an input,
+      chooses the most common class out of the k nearest data points to that input.
+
+    *vector-mathematics :
+      h(x1,x2)= 1 if w0 + w1x1=w2x2 > or = 0
+                0 otherwise
+
+        wwight-vector w: (w0,w1,w2) 
+        input vector x:(1,x1,x2)
+        w . x: w0 + w1x1 +w2x2
+           (h represent hypothesis)
+            hw(x)= 1 if w.x > or = 0
+            0 otherwise
+
+        perceptron learning rule:
+        given data point (x,y)(x is deta and y result of data), update each weight
+        according to :
+         wi = wi + a(actual value - estimate) * xi
+
+    '''
