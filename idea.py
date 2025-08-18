5589991c213f3fae48fea72c7a0822ecc24623d7
shatinz <shatincia@gmail.com>
2025-08-18 14:01:16 -0700

    learning

---
+++ idea.py
@@ -0,0 +1,8 @@
+        idea: make an algorythem that works for only estimating margins and out of line datas.
+        making a n dimentional ground and show where data is in that groung and make machine to learn the 
+        relationships.
+         
+
+         today we need gathering data teckniques more than ever.
+
+        sklearn
--- learning.py
+++ learning.py
@@ -40,4 +40,32 @@
 
     regression:
         supervised learning task of learning a function mapping an input point to a continuous value.
+        function(input)
+          f(1200)=5800
+          f(500)=13400
+        hypothesis(input)
+    
+    EVALUATING HYPOTHESIS:
+      loss function : function that express how poorly our hypothesis perform.
+        0-1 loss function(for discrete values) :
+        l(actual, predict)=
+          0 if actual = predict
+          1 otherwise
+            L1loss function(how far apart where the actual value and predicted values):
+            L(actual,predicted)=|actual-predict|
+            l2 loss function(effectively penaliing much more harshly any thing that is worse prediction):
+            L(actual,predict)=(actual-predicted)2
+        OVERFITTING :
+        a model that fits so closely to a particular data set and therefore may fail generalize to future data.
+          regularizatio:
+            penalizing pypothesis that are more complex to favor simpler, more general hypotheses.
+            cost(h) = loss(h) + complexity(h)
+          holdout cross-validation:
+            splitting data into a training set and a test set, such is trainnig happens on the training set,
+            and is evaluated on the test set.
+          k-fold cross-validation:
+          splitting data into k sets, and experimenting k times, using each set as a test once,
+          and using remaining data as training set. 
+    reinforcement learning:
+      given a set of rewards or punishments, learn what action to take in in  future
     '''
